cdb83a5c8d7c4d39c55cff612a74a15d021a7683
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2023-12-25 17:59:16 +0530

    Add files via upload

---
+++ Average_Temp.py
@@ -0,0 +1,17 @@
+
+# Function for finding determinant of matrix.
+class Solution:
+
+
+    def determinantOfMatrix(self, matrix, n):
+
+        if n == 1:
+            return matrix[0][0]
+        if n == 2:
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+        ans = 0
+        for i in range(n):
+            mat = [matrix[j][1:] for j in range(n) if j != i]
+            ans += ((-1) ** (i)) * matrix[i][0] * self.determinantOfMatrix(mat, n - 1)
+        return ans
+
